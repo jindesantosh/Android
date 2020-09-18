cc0a0e52550759dc785eb95668d7be27f62ad21a
jindesantosh <31130974+jindesantosh@users.noreply.github.com>
2020-09-18 14:53:16 +0530

    Create HomePage.dart

---
+++ HomePage.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // ignore: todo
+  //TODO: Link Up Images
+  AssetImage cross = AssetImage("images/cross.png");
+  AssetImage circle = AssetImage("images/circle.png");
+  AssetImage edit = AssetImage("images/edit.png");
+
+  bool isCross = true;
+  String message;
+  List<String> gameState;
+
+  // ignore: todo
+  //TODO: Initialize the state of box with Empty
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      this.gameState = [
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+      ];
+      this.message = "";
+    });
+  }
+
+  // ignore: todo
+  //TODO: PlayGame Meathod
+  playGame(int index) {
+    if (this.gameState[index] == "empty") {
+      setState(() {
+        if (this.isCross) {
+          // ignore: unnecessary_statements
+          this.gameState[index] == "cross";
+        } else {
+          // ignore: unnecessary_statements
+          this.gameState[index] == "circle";
+        }
+        this.isCross = !this.isCross;
+        this.checkWin();
+      });
+    }
+  }
+
+  // ignore: todo
+  //TODO: Reset Game Meathod
+  resetGame() {
+    setState(() {
+      this.gameState = [
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+      ];
+      this.message = "";
+    });
+  }
+
+  // ignore: todo
+  //TODO: Get Image Meathod
+  // ignore: missing_return
+  AssetImage getImage(String value) {
+    switch (value) {
+      case ('empty'):
+        return edit;
+        break;
+      case ('cross'):
+        return cross;
+        break;
+      case ('circle'):
+        return circle;
+        break;
+    }
+  }
+
+  // ignore: todo
+  //TODO: Check for win Logic
+  checkWin() {
+    if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[1]) &&
+        (gameState[1] == gameState[2])) {
+      setState(() {
+        this.message = '${this.gameState[0]} Wins';
+      });
+    } else if ((gameState[3] != 'empty') &&
+        (gameState[3] == gameState[4]) &&
+        (gameState[4] == gameState[5])) {
+      setState(() {
+        this.message = '${this.gameState[3]} Wins';
+      });
+    } else if ((gameState[6] != 'empty') &&
+        (gameState[6] == gameState[7]) &&
+        (gameState[7] == gameState[8])) {
+      setState(() {
+        this.message = '${this.gameState[6]} Wins';
+      });
+    } else if ((gameState[2] != 'empty') &&
+        (gameState[2] == gameState[4]) &&
+        (gameState[4] == gameState[6])) {
+      setState(() {
+        this.message = '${this.gameState[2]} Wins';
+      });
+    } else if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[4]) &&
+        (gameState[4] == gameState[8])) {
+      setState(() {
+        this.message = '${this.gameState[0]} Wins';
+      });
+    } else if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[3]) &&
+        (gameState[3] == gameState[6])) {
+      setState(() {
+        this.message = '${this.gameState[0]} Wins';
+      });
+    } else if ((gameState[1] != 'empty') &&
+        (gameState[1] == gameState[4]) &&
+        (gameState[4] == gameState[7])) {
+      setState(() {
+        this.message = '${this.gameState[1]} Wins';
+      });
+    } else if ((gameState[2] != 'empty') &&
+        (gameState[2] == gameState[5]) &&
+        (gameState[5] == gameState[8])) {
+      setState(() {
+        this.message = '${this.gameState[2]} Wins';
+      });
+    } else if (!gameState.contains('empty')) {
+      setState(() {
+        this.message = "Game Draw";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tic Tac Toe By Santosh'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: GridView.builder(
+              padding: EdgeInsets.all(20.0),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0),
+              itemCount: this.gameState.length,
+              itemBuilder: (context, i) => SizedBox(
+                width: 100.0,
+                height: 200.0,
+                child: MaterialButton(
+                  onPressed: () {
+                    this.playGame(i);
+                  },
+                  child: Image(
+                    image: this.getImage(this.gameState[i]),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              this.message,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          MaterialButton(
+            color: Colors.purple,
+            minWidth: 300.0,
+            height: 50.0,
+            child: Text(
+              "Reset Game",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
+            onPressed: () {
+              this.resetGame();
+            },
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Text("Design By Santosh", style: TextStyle(fontSize: 18.0)),
+          )
+        ],
+      ),
+    );
+  }
+}
